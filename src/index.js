c6bd2a976df80fb5fb6e26a9adcfdaa27f8a2669
Nathalia Duarte <duartenatha@gmail.com>
2022-10-01 11:56:46 -0300

    fix index

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/pessoa", pessoaRoutes);
 app.use(
   "/files",
   express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
 );
 
 const pessoaRoutes = require("./routes/pessoaRoutes");
+app.use("/pessoa", pessoaRoutes);
 
 mongoose
   .connect(process.env.DB_URL, {
