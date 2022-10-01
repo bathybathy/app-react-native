0c0b46db19a81c88bb5c9bafeaa45e8f3c4658e8
Nathalia Duarte <duartenatha@gmail.com>
2022-10-01 13:15:35 -0300

    fix deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 //const pessoaRoutes = require("./routes/pessoaRoutes");
