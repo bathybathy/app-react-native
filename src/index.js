e58a2e004fb54c4a0cc2e5a05ee245bf7f7328bb
Nathalia Duarte <duartenatha@gmail.com>
2022-10-01 11:58:19 -0300

    fix faltando require

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
