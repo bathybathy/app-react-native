52d5609af43a9222d2bf0ac7854de25af88f92cf
Nathalia Duarte <duartenatha@gmail.com>
2022-10-01 11:50:37 -0300

    mais fix deploy

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
+app.use("/pessoa", pessoaRoutes);
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "tmp", "uploads"))
+);
 
 const pessoaRoutes = require("./routes/pessoaRoutes");
-
-app.use("/pessoa", pessoaRoutes);
 
 mongoose
   .connect(process.env.DB_URL, {
